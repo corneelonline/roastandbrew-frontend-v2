07508b821233d039d489692e9bff8b24f21ae333
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-13 10:36:35 +0200

    Add companies API and use it in the roastAPI plugin

--- plugins/roastAPI.js
+++ plugins/roastAPI.js
@@ -1,6 +1,8 @@
+import CompaniesAPI from '@/api/companies.js';
+
 export default function( { $axios }, inject ){
   const api = {
-
+    companies: CompaniesAPI( $axios )
   }
 
   inject('api', api);
