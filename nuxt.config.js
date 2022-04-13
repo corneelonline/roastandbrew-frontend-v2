9e3d6006331276cdcb1bdb28e9229ebd3caa087b
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-13 10:25:20 +0200

    Add axios configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,8 +42,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.API_BASE_URL,
+    credentials: true
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
