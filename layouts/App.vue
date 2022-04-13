f5aadd4c2fb6cd61c9647c02f0b6602caf07ac20
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-13 11:27:47 +0200

    Add basic app layout and empty header and footer components

---
+++ layouts/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app-layout">
+    <div>
+      <app-header v-show="showHeader"/>
+    </div>
+    <main>
+      <nuxt />
+    </main>
+    <app-footer v-show="showFooter" />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/global/Header/AppHeader.vue';
+import AppFooter from '@/components/global/Footer/AppFooter.vue';
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    AppHeader,
+    AppFooter
+  },
+  computed: {
+    showFooter(){
+      let pages = [];
+      return pages.indexOf( this.$route.name ) == -1;
+    },
+    showHeader(){
+      let pages = [];
+      return pages.indexOf( this.$route.name ) == -1;
+    },
+  }
+}
+</script>
