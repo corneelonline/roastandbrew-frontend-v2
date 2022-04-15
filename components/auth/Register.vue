9814a67c1ea64cf0258b55c123af5f0d2e898c15
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-15 12:28:26 +0200

    Add registration component - in progress

---
+++ components/auth/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="register">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        confirm_password: ''
+      },
+      validations: {
+        name: {
+          valid: true,
+          message: ''
+        },
+        email: {
+          valid: true,
+          message: ''
+        },
+        password: {
+          valid: true,
+          message: ''
+        },
+        confirm_password: {
+          valid: true,
+          message: ''
+        }
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
